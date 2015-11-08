4cdbb66570b5356a681351b9452c4243960c93b5
deZakelijke <git@michadegroot.nl>
2015-11-08 09:06:50 -0800

    Update multivariateLinearRegression.py

--- multivariateLinearRegression.py
+++ multivariateLinearRegression.py
@@ -1,6 +1,6 @@
-#20151101 H0mework assignment University of Amsterdam
-#First assignment for the course 'leren' of bachelor AI
-#Made by Micha de Groot, student number 10434410
+#20151108 H0mework assignment University of Amsterdam
+#Second assignment for the course 'leren' of bachelor AI
+#Made by Micha de Groot, student number 10434410 and Willemijn Beks, student number 10775110
 
 import csv
 import sys
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 #Read the csv file and convert to right data format.
 def useDataFile():
-    arr = [[]]
+    arr = [[],[],[],[],[]]
     with open('housesRegr.csv', 'rU') as f:
         reader = csv.reader(f, dialect=csv.excel_tab)
         next(reader)
@@ -28,9 +28,9 @@ def convertToMatrix(data):
     n = len(data)-1
     m = len(data[0])
     matrix = np.empty([m,n])
-    for i in xrange(data):
-        for j in xrange(data[0])
-            matrix[i][j]=data[j][i]
+    for i in xrange(n):
+        for j in xrange(m):
+            matrix[j][i]=data[i][j]
     return matrix
 
 #Plot the dataset and the function in two dimensions
@@ -47,60 +47,64 @@ def plotData(data, dataType, theta0, theta1):
 
 #Calculathe the hypothesis 
 def hypothesis(theta, x):
-    return theta.dot(x)
+    return np.dot(theta,x)
 
 #Calculate the cost 
-def cost(theta, data):
-    m = len(data[0])
-    price = 0.0
+def cost(theta, x, y):
+    n = len(x)+1
+    m = len(x[0])
+    cost = 0.0
     for i in xrange(m):
-        price += (hypothesis(theta0, theta1, data[dataType][i]) - data[0][i])**2
-    return price /(2*m)
+        cost += (hypothesis(theta, x[i]) - y[m])**2
+    return cost /(2*m)
 
-#Gradient function for theta0
-def gradientTheta0(theta0, theta1, data, dataType):
-    m =  len(data[0])
+#Gradient function for thetaJ
+def gradientTheta(theta, x, y, thetaIndex):
+    m =  len(x[0])
     gradient = 0.0
     for i in xrange(m):
-        gradient += hypothesis(theta0, theta1, data[dataType][i]) - data[0][i]
+        gradient += (hypothesis(theta, x[i]) - y[i])*x[i][thetaIndex]
     return gradient/m
 
-#Update theta0
-def updateTheta0(alpha, theta0, theta1, data, dataType):
-     return theta0-alpha * gradientTheta0(theta0, theta1, data, dataType)
-
-#Iterate to update theta0 and theta1
-def updateParameters(iterations, alpha, theta0, theta1, data, dataType):
-    tempTheta0 = theta0
+#Update theta
+def updateTheta(iterations, alpha, theta, x, y):
+    tempTheta = theta
     for i in xrange(iterations):
-        theta0 = updateTheta0(alpha, theta0, theta1, data, dataType)
-        theta1 = updateTheta1(alpha, tempTheta0, theta1, data, dataType)
-        tempTheta0 = theta0
-    return [theta0, theta1]
+        for j in xrange(len(theta)):   
+            tempTheta[j] = theta[j]-alpha * gradientTheta(theta, x, y, j)
+        theta = tempTheta
+    return theta
 
-#main program
-iterations = 10
-alpha = 0.001
-theta = np.array([0.0, 1.0, 1.0, 1.0])
+def multiple(iterations, alpha, theta):
+
+    print 'number of iterations is set to: ', iterations
+    print 'alpha value is set to: ', alpha
+
+    data = useDataFile()
+    y = np.array(data[4])
+    dataMatrix = convertToMatrix(data[0:5])
+
+    oldCost = cost(theta, dataMatrix, y)
+    print 'Old cost is: ', oldCost
+    for i in xrange(len(theta)):
+        print 'Old theta',i ,' is: ', theta[i]
+
+    newTheta = updateTheta(iterations, alpha, theta, dataMatrix, y)
+    newCost = cost(theta, dataMatrix, y)
+    print 'New cost is: ', newCost
+    for i in xrange(len(theta)):
+        print 'New theta',i ,' is: ', theta[i]
+
+    print 'Cost improvement is: ', oldCost - newCost
+
+
+iterations = 10000
+alpha = 0.000000000001
+theta = np.array([0.0, 1.0, 1.0, 10.0])
 
 if len(sys.argv)>1:
     iterations = int(sys.argv[1])
 
 if len(sys.argv)>2:
     alpha = float(sys.argv[2])
-
-print 'number of iterations is set to: ', iterations
-print 'alpha value is set to: ', alpha
-
-data = useDataFile()
-y = np.array(data[4])
-dataMatrix = convertToMatrix(data[0:3])
-
-print 'Old cost is: ', cost(theta0, theta1, data, dataType)
-newParam = updateParameters(iterations, alpha, theta0, theta1, data, dataType)
-theta0 = newParam[0]
-theta1 = newParam[1]
-print 'New theta0 is: ', theta0
-print 'New theta1 is: ', theta1
-print 'New cost is: ', cost(theta0, theta1, data, dataType)
-plotData(data,dataType, theta0, theta1)
+multiple(iterations, alpha, theta)
